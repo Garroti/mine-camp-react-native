d0a3cb2b2b8b4e2ae564251b4acec751049279db
Garroti <gabryelmacedo0@gmail.com>
2021-01-25 22:05:33 -0300

    campo minado completo

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native'
 import params from '../params'
 import Mine from './Mine'
+import Flag from './Flag'
 
 export default props => {
 
-  const { mined, opened, nearMines } = props
+  const { mined, opened, nearMines, exploded, flagged } = props
 
   const styleField = [styles.field]
   if(opened) styleField.push(styles.opened)
   if(exploded) styleField.push(styles.exploded)
-  if(styleField.length === 1) styleField.push(styles.regular)
+  if(flagged) styleField.push(styles.flagged)
+  if(!opened && !exploded) styleField.push(styles.regular)
 
   let color = null
   if(nearMines > 0){
@@ -21,14 +23,22 @@ export default props => {
   }
 
   return (
-    <View style={ styleField }>
-      { 
-        !mined && opened && nearMines > 0 ? 
-        <Text style={ [styles.label, { color: color }] }>
-          { nearMines }
-        </Text> : false
-      }
-    </View>
+    <TouchableWithoutFeedback onPress={ props.onOpen } onLongPress={ props.onSelect }>
+      <View style={ styleField }>
+        { 
+          !mined && opened && nearMines > 0 ? 
+          <Text style={ [styles.label, { color: color }] }>
+            { nearMines }
+          </Text> : false
+        }
+        {
+          mined && opened ? <Mine /> : false
+        }
+        {
+          flagged && !opened ? <Flag /> : false 
+        }
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -43,16 +53,20 @@ const styles = StyleSheet.create({
     borderLeftColor: '#CCC',
     borderTopColor: '#CCC',
     borderRightColor: '#333',
-    borderBottomColor: '#333'
+    borderBottomColor: '#333',
   },
   opened: {
     backgroundColor: '#999',
     borderColor: '#777',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   label: {
     fontWeight: 'bold',
     fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red',
   }
 })
